705334d6e3c738938ad4fcef822e5df2c95e0d43
T01764 <pedro.rodrigues@taking.com.br>
2023-10-02 17:02:21 -0300

    update project

---
+++ README.md
@@ -0,0 +1,4 @@
+para rodar o projeto
+    streamlit run dashboards.py
+
+![img.png](img.png)
--- dashboards.py
+++ dashboards.py
@@ -1,6 +1,6 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
 st.set_page_config(layout="wide")
 
